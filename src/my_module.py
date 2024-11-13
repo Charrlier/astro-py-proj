b9a1d4081249d9ae5d36d25c515408dc47c53453
Charrlier <Quentin.BONHAURE@estaca.eu>
2024-11-13 16:57:52 +0100

    Update my_module.py

--- src/my_module.py
+++ src/my_module.py
@@ -1,1 +1,18 @@
+import click
+import matplotlib.pyplot as plt
 
+@click.command()
+@click.option('--name', prompt='Your name', help='The user\'s name.')
+def greet(name):
+    """Simple program that greets NAME."""
+    click.echo(f"Hello, {name}!")
+
+def display_chart():
+    """Displays a simple chart."""
+    plt.plot([1, 2, 3], [1, 4, 9])
+    plt.title("Sample Chart")
+    plt.show()
+
+if __name__ == "__main__":
+    greet()
+    display_chart()
