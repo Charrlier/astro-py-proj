e3d46c710760bae07e27dfe9bf3d2c742087cb4c
Charrlier <Quentin.BONHAURE@estaca.eu>
2024-11-13 16:58:09 +0100

    Update test_my_module.py

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -1,1 +1,10 @@
+from src import my_module
 
+def test_greet(capsys):
+    my_module.greet("Tester")
+    captured = capsys.readouterr()
+    assert "Hello, Tester!" in captured.out
+
+def test_display_chart():
+    # Here you would add tests for display_chart functionality
+    pass
